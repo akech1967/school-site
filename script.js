02e3ccc1ea3615a62f33495f75be2dd92c6f9b12
akech1967 <avtandil.kechakmadze@gmail.com>
2025-02-23 23:53:55 +0400

    add pages

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split("/").pop(); // იღებს მიმდინარე ფაილის სახელს (მაგ., index.html)
+    const menuLinks = document.querySelectorAll(".nav-menu a");
+  
+    menuLinks.forEach(link => {
+      const linkPage = link.getAttribute("href").split("/").pop(); // იღებს მხოლოდ ფაილის სახელს ლინკიდან
+      if (linkPage === currentPage) {
+        link.classList.add("active"); // ამატებს active კლასს სწორ ლინკს
+      }
+    });
+  });
+  
+  
+
+  
